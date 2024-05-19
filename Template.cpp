ca04fb46961a5ffd1d54ece210ca15a4d6fe33c9
wolgwang1729 <145632038+wolgwang1729@users.noreply.github.com>
2024-05-19 12:09:52 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x << " ";  _print(x);   cerr << endl;
+#define debug2(x,y) cerr << #x << " ";  _print(x); cerr <<" "<<#y<< " ";  _print(y);  cerr << endl;
 #else
 #define debug(x)
+#define debug2(x,y) 
 #endif
 #define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
 #define MOD 1000000007
@@ -45,25 +47,31 @@ void _print(ull t) { cerr << t; }
 template <class T, class V>
 void _print(pair<T, V> p);template <class T>
 void _print(vector<T> v1);template <class T>
+void _print(deque<T> v1);template <class T>
 void _print(set<T> v1);template <class T, class V>
 void _print(map<T, V> v1);template <class T>
 void _print(multiset<T> v1);template <class T, class V>
 void _print(pair<T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
 template <class T>void _print(vector<T> v1){cerr << "[ ";for (T i : v1){_print(i);cerr << " ";}cerr << "]";}
+template <class T>void _print(deque<T> v1){cerr << "[ ";for (T i : v1){_print(i);cerr << " ";}cerr << "]";}
 template <class T>void _print(set<T> v1){cerr << "[ ";for (T i : v1){_print(i);cerr << " ";}cerr << "]";}
 template <class T>void _print(multiset<T> v1){cerr << "[ ";for (T i : v1){_print(i);cerr << " ";}cerr << "]";}
 template <class T, class V>void _print(map<T, V> v1){cerr << "[ ";for (auto i : v1){_print(i);cerr << " ";} cerr << "]";}
 
+
+
+void solve() {
+}
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("Error.txt", "w", stderr);
 #endif
     fastio();
-    int t = 1;
-    cin >> t;
-    while (t--)
+    int testcases = 1;
+    cin >> testcases;
+    while (testcases--)
     {
-        
+        solve(); 
     }
 }
